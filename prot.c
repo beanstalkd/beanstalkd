8b0e9bdfd91509fa01b21e72e1a2007052028b0c
Dustin Sallings <dustin@spy.net>
2008-06-09 22:35:36 -0700

    Fix buffer underrun in stats printing.
    This is a race condition, but, for example, if the uptime wraps
    in the middle of a do_stats call.
    
    The easiest way to contrive a test for this is to throw a sleep in the
    middle of do_stats.

--- prot.c
+++ prot.c
@@ -915,18 +915,19 @@ do_stats(conn c, fmt_fn fmt, void *data)
     int r, stats_len;
 
     /* first, measure how big a buffer we will need */
-    stats_len = fmt(NULL, 0, data);
+    stats_len = fmt(NULL, 0, data) + 16;
 
     c->out_job = allocate_job(stats_len); /* fake job to hold stats data */
     if (!c->out_job) return reply_serr(c, MSG_OUT_OF_MEMORY);
 
     /* now actually format the stats data */
     r = fmt(c->out_job->body, stats_len, data);
-    if (r != stats_len) return reply_serr(c, MSG_INTERNAL_ERROR);
-    c->out_job->body[stats_len - 1] = '\n'; /* patch up sprintf's output */
+    /* and set the actual body size */
+    c->out_job->body_size = r;
+    if (r > stats_len) return reply_serr(c, MSG_INTERNAL_ERROR);
 
     c->out_job_sent = 0;
-    return reply_line(c, STATE_SENDJOB, "OK %d\r\n", stats_len - 2);
+    return reply_line(c, STATE_SENDJOB, "OK %d\r\n", r - 2);
 }
 
 static void
