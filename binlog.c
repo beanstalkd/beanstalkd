6741cc9db9d3c7977383059600cddb78c2609ace
Graham Barr <gbarr@pobox.com>
2008-11-05 09:06:32 -0600

    Update Copyright

--- binlog.c
+++ binlog.c
@@ -1,6 +1,6 @@
 /* binlog.c - binary log implementation */
 
-/* Copyright (C) 2007 Keith Rarick and Philotic Inc.
+/* Copyright (C) 2008 Graham Barr
 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
