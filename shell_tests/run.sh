16860563e9349e901a824b4642b2bc8819ed965d
Kristjan <kristjan@causes.com>
2008-11-13 19:33:21 -0800

    Clean up test harness

--- shell_tests/run.sh
+++ shell_tests/run.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+echo "Starting Tests..."
 for commands in shell_tests/*.commands; do
   expected=${commands/.commands/.expected}
-  echo "Testing ${commands/.commands/}"
-  shell_tests/run_one.sh $commands $expected || exit 1
+  echo Testing $(echo $commands | sed -re 's/.*\/(.*)\..*/\1/')
+  shell_tests/run_one.sh $commands $expected
+  if test $? != 0; then
+    echo "!!! TEST FAILED !!!"
+    exit 1
+  fi
 done 
+echo -e "\r\n***All tests passed***\r\n"
--- shell_tests/run_one.sh
+++ shell_tests/run_one.sh
@@ -1,26 +1,27 @@
 #!/bin/bash
 
 SERVER='localhost'
-PORT=11300
-./beanstalkd 2> /dev/null & 
+PORT=11400
+./beanstalkd -p $PORT >/dev/null 2>/dev/null &
+bg=$!
+
 sleep .1
-if ! test -e "/proc/$!"; then
-  echo "Could not start beanstalkd for testing"
+if ! ps $bg >/dev/null; then
+  echo "Could not start beanstalkd for testing, port $PORT is taken"
   exit 1
 fi
 
 clean_exit() {
-  kill $! 2> /dev/null
+  kill -9 $1
   rm .tmp_test
-  exit $1
+  exit $2
 }
 
 fgrep -v "#" $1 | nc $SERVER $PORT > .tmp_test
 
 # diff is "true" if they match
 if diff .tmp_test $2; then
-  clean_exit 0
+  clean_exit $bg 0 2>/dev/null
 fi
 
-clean_exit 1
-
+clean_exit $bg 1 2>/dev/null
