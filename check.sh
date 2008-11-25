c3f70c4214531964208e6aa0b135aadc683b110e
Keith Rarick <kr@xph.us>
2008-11-25 15:52:38 -0800

    Only print the fail message if the test failed.

--- check.sh
+++ check.sh
@@ -7,9 +7,8 @@ for commands in "$@"; do
   expected=${commands/.commands/.expected}
   echo Testing $(echo $commands | sed -re 's/.*\/(.*)\..*/\1/')
   $one $commands $expected
-  if test $? != 0; then
-    echo "!!! TEST FAILED !!!"
-    exit 1
-  fi
+  res=$?
+  test "$res" = 1 && echo "FAIL: $commands"
+  test "$res" = 0 || exit 1
 done 
 echo -e "\r\n***All tests passed***\r\n"
