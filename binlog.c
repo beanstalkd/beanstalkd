6e0675e2a610bdc14315eee04080516c768468ac
Keith Rarick <kr@xph.us>
2009-11-17 15:32:13 -0800

    Quiet compiler warning about unused return value.
    We don't really care if the ftruncate call fails, it just means
    beanstalkd might report warnings next time it starts up.

--- binlog.c
+++ binlog.c
@@ -261,7 +261,7 @@ binlog_close(binlog b)
 {
     if (!b) return;
     if (b->fd < 0) return;
-    if (b->free) ftruncate(b->fd, lseek(b->fd, b->free, SEEK_END));
+    if (b->free) 0 && ftruncate(b->fd, lseek(b->fd, b->free, SEEK_END));
     close(b->fd);
     b->fd = -1;
     binlog_dref(b);
