ca11f682e543dd3153b30c317727edb60c5eedc1
Keith Rarick <kr@xph.us>
2011-12-03 17:04:56 -0800

    read version 5 of the log format

--- file.c
+++ file.c
@@ -1,4 +1,5 @@
 #include <stdint.h>
+#include <stddef.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
@@ -12,8 +13,38 @@
 
 static void warnpos(File*, int, char*, ...);
 static int  readrec(File*, job, int*);
+static int  readrec5(File*, job, int*);
 static int  readfull(File*, void*, int, int*, char*);
 
+enum
+{
+    Walver5 = 5
+};
+
+typedef struct Jobrec5 Jobrec5;
+
+struct Jobrec5 {
+    uint64 id;
+    uint32 pri;
+    uint64 delay; // usec
+    uint64 ttr; // usec
+    int32  body_size;
+    uint64 created_at; // usec
+    uint64 deadline_at; // usec
+    uint32 reserve_ct;
+    uint32 timeout_ct;
+    uint32 release_ct;
+    uint32 bury_ct;
+    uint32 kick_ct;
+    byte   state;
+
+    char pad[1];
+};
+
+enum
+{
+	Jobrec5size = offsetof(Jobrec5, pad)
+};
 
 void
 fileincref(File *f)
@@ -76,15 +107,21 @@ fileread(File *f, job list)
     if (!readfull(f, &v, sizeof(v), &err, "version")) {
         return err;
     }
-    if (v != Walver) {
-        warnx("%s: wrong version: want %d, got %d", f->path, Walver, v);
-        return 1;
+    switch (v) {
+    case Walver:
+        fileincref(f);
+        while (readrec(f, list, &err));
+        filedecref(f);
+        return err;
+    case Walver5:
+        fileincref(f);
+        while (readrec5(f, list, &err));
+        filedecref(f);
+        return err;
     }
 
-    fileincref(f);
-    while (readrec(f, list, &err));
-    filedecref(f);
-    return err;
+    warnx("%s: unknown version: %d", f->path, v);
+    return 1;
 }
 
 
@@ -171,6 +208,135 @@ readrec(File *f, job l, int *err)
                 goto Error;
             }
             r = readfull(f, j->body, j->r.body_size, err, "job body");
+            if (!r) {
+                goto Error;
+            }
+            sz += r;
+
+            // since this is a full record, we can move
+            // the file pointer and decref the old
+            // file, if any
+            filermjob(j->file, j);
+            fileaddjob(f, j);
+        }
+        j->walused += sz;
+        f->w->alive += sz;
+
+        return 1;
+    case Invalid:
+        if (j) {
+            job_remove(j);
+            filermjob(j->file, j);
+            job_free(j);
+        }
+        return 1;
+    }
+
+Error:
+    *err = 1;
+    if (j) {
+        job_remove(j);
+        filermjob(j->file, j);
+        job_free(j);
+    }
+    return 0;
+}
+
+
+// Readrec5 is like readrec, but it reads a record in "version 5"
+// of the log format.
+static int
+readrec5(File *f, job l, int *err)
+{
+    int r, sz = 0;
+    size_t namelen;
+    Jobrec5 jr;
+    job j;
+    tube t;
+    char tubename[MAX_TUBE_NAME_LEN];
+
+    r = read(f->fd, &namelen, sizeof(namelen));
+    if (r == -1) {
+        twarn("read");
+        warnpos(f, 0, "error");
+        *err = 1;
+        return 0;
+    }
+    if (r != sizeof(namelen)) {
+        return 0;
+    }
+    sz += r;
+    if (namelen >= MAX_TUBE_NAME_LEN) {
+        warnpos(f, -r, "namelen %d exceeds maximum of %d", namelen, MAX_TUBE_NAME_LEN - 1);
+        *err = 1;
+        return 0;
+    }
+
+    if (namelen) {
+        r = readfull(f, tubename, namelen, err, "v5 tube name");
+        if (!r) {
+            return 0;
+        }
+        sz += r;
+    }
+    tubename[namelen] = '\0';
+
+    r = readfull(f, &jr, Jobrec5size, err, "v5 job struct");
+    if (!r) {
+        return 0;
+    }
+    sz += r;
+
+    // are we reading trailing zeroes?
+    if (!jr.id) return 0;
+
+    j = job_find(jr.id);
+    if (!(j || namelen)) {
+        // We read a short record without having seen a
+        // full record for this job, so the full record
+        // was in an eariler file that has been deleted.
+        // Therefore the job itself has either been
+        // deleted or migrated; either way, this record
+        // should be ignored.
+        return 1;
+    }
+
+    switch (jr.state) {
+    case Reserved:
+        jr.state = Ready;
+    case Ready:
+    case Buried:
+    case Delayed:
+        if (!j) {
+            t = tube_find_or_make(tubename);
+            j = make_job_with_id(jr.pri, jr.delay, jr.ttr, jr.body_size,
+                                 t, jr.id);
+            j->next = j->prev = j;
+            j->r.created_at = jr.created_at;
+        }
+        j->r.id = jr.id;
+        j->r.pri = jr.pri;
+        j->r.delay = jr.delay * 1000; // us => ns
+        j->r.ttr = jr.ttr * 1000; // us => ns
+        j->r.body_size = jr.body_size;
+        j->r.created_at = jr.created_at * 1000; // us => ns
+        j->r.deadline_at = jr.deadline_at * 1000; // us => ns
+        j->r.reserve_ct = jr.reserve_ct;
+        j->r.timeout_ct = jr.timeout_ct;
+        j->r.release_ct = jr.release_ct;
+        j->r.bury_ct = jr.bury_ct;
+        j->r.kick_ct = jr.kick_ct;
+        j->r.state = jr.state;
+        job_insert(l, j);
+
+        // full record; read the job body
+        if (namelen) {
+            if (jr.body_size != j->r.body_size) {
+                warnpos(f, -r, "job %llu size changed", j->r.id);
+                warnpos(f, -r, "was %zu, now %zu", j->r.body_size, jr.body_size);
+                goto Error;
+            }
+            r = readfull(f, j->body, j->r.body_size, err, "v5 job body");
             if (!r) {
                 goto Error;
             }
---
+++ sh-tests/binlog-v5.sh
@@ -0,0 +1,126 @@
+#!/usr/bin/env bash
+
+. "sh-tests/common.functions"
+
+server=localhost
+tmpdir="$TMPDIR"
+test -z "$tmpdir" && tmpdir=/tmp
+out1="${tmpdir}/bnch$$.1"
+out2="${tmpdir}/bnch$$.2"
+logdir="${tmpdir}/bnch$$.d"
+nc="sh-tests/netcat.py"
+
+cleanup() {
+    killbeanstalkd
+    rm -rf "$logdir" "$out1" "$out2"
+}
+
+catch() {
+    echo '' Interrupted
+    exit 3
+}
+
+trap cleanup EXIT
+trap catch HUP INT QUIT TERM
+
+if [ ! -x ./beanstalkd ]; then
+  echo "Executable ./beanstalkd not found; do you need to compile first?"
+  exit 2
+fi
+
+if ! type beanstalkd-1.4.6 >/dev/null
+then echo beanstalkd 1.4.6 not found -- skipping binlog-v5. ; exit 0
+fi
+start_beanstalkd $logdir '' '' beanstalkd-1.4.6
+
+$nc $server $port <<EOF |egrep -v "age|time-left" >"$out1"
+use test
+put 1 2 3 4
+test
+put 4 3 2 1
+x
+stats-job 1
+stats-job 2
+quit
+EOF
+
+diff - "$out1" <<EOF
+USING test
+INSERTED 1
+INSERTED 2
+OK 134
+---
+id: 1
+tube: test
+state: delayed
+pri: 1
+delay: 2
+ttr: 3
+reserves: 0
+timeouts: 0
+releases: 0
+buries: 0
+kicks: 0
+
+OK 134
+---
+id: 2
+tube: test
+state: delayed
+pri: 4
+delay: 3
+ttr: 2
+reserves: 0
+timeouts: 0
+releases: 0
+buries: 0
+kicks: 0
+
+EOF
+res=$?
+test "$res" -eq 0 || exit $res
+
+killbeanstalkd
+
+sleep 1
+start_beanstalkd $logdir
+
+$nc $server $port <<EOF |egrep -v "age|time-left" >"$out2"
+stats-job 1
+stats-job 2
+quit
+EOF
+
+diff - "$out2" <<EOF
+OK 142
+---
+id: 1
+tube: test
+state: delayed
+pri: 1
+delay: 2
+ttr: 3
+file: 1
+reserves: 0
+timeouts: 0
+releases: 0
+buries: 0
+kicks: 0
+
+OK 142
+---
+id: 2
+tube: test
+state: delayed
+pri: 4
+delay: 3
+ttr: 2
+file: 1
+reserves: 0
+timeouts: 0
+releases: 0
+buries: 0
+kicks: 0
+
+EOF
+
