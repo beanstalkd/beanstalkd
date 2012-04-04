f760e2d377aaa80a0b6bb0115359c9e7db1e3189
Keith Rarick <kr@xph.us>
2012-04-04 01:27:03 -0700

    update ct

--- ct/ct.h
+++ ct/ct.h
@@ -2,11 +2,11 @@ void ctfail(void);
 void ctlogpn(char*, int, char*, ...);
 #define ctlog(...) ctlogpn(__FILE__, __LINE__, __VA_ARGS__)
 #define assert(x) do if (!(x)) {\
-	ctlog("test: " #x);\
+	ctlog("%s", "test: " #x);\
 	ctfail();\
 } while (0)
 #define assertf(x, ...) do if (!(x)) {\
-	ctlog("test: " #x);\
+	ctlog("%s", "test: " #x);\
 	ctlog(__VA_ARGS__);\
 	ctfail();\
 } while (0)
