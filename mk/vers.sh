786bda752e5f6303d4517601f9c6f128fa47f540
Isaac Jurado <diptongo@gmail.com>
2012-09-13 20:23:36 +0200

    Tolerate compilation without a Git repository.

--- mk/vers.sh
+++ mk/vers.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
-git describe | sed s/^v// | tr - + | tr -d '\n'
-if ! git diff --quiet HEAD
-then printf +mod
+if git describe >/dev/null 2>&1
+then
+    git describe | sed s/^v// | tr - + | tr -d '\n'
+    if ! git diff --quiet HEAD
+    then printf +mod
+    fi
+else
+    printf unknown
 fi
