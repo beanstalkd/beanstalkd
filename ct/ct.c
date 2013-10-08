cd624834e261f6c9dda866a9db9f7ff5920624c4
Daniel Beardsley <daniel@ifixit.com>
2013-10-07 23:45:03 -0700

    Unit Tests: one more CentOS compatability fix
    Apparently C has macros for 64bit printf formats across different system
    architectures.
    
    See this SO answer: http://stackoverflow.com/a/9225648/13216

--- ct/ct.c
+++ ct/ct.c
@@ -15,6 +15,7 @@
 #include <time.h>
 #include <sys/time.h>
 #include <stdint.h>
+#include <inttypes.h>
 #include "internal.h"
 #include "ct.h"
 
@@ -417,7 +418,7 @@ runbench(Benchmark *b)
         runbenchn(b, n);
     }
     if (b->status == 0) {
-        printf("%8d\t%10lld ns/op", n, (long long)b->dur/n);
+        printf("%8d\t%10" PRId64 " ns/op", n, b->dur/n);
         if (b->bytes > 0) {
             double mbs = 0;
             if (b->dur > 0) {
