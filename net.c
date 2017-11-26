bc4582588b6fb8418f14663dde815134de4c2af7
Matthew King <chohag@jtan.com>
2017-11-27 00:21:05 +0200

    Report bound port correctly

--- net.c
+++ net.c
@@ -98,27 +98,34 @@ make_server_socket(char *host, char *port)
         continue;
       }
 
-      if (verbose) {
-          char hbuf[NI_MAXHOST], pbuf[NI_MAXSERV], *h = host, *p = port;
-          r = getnameinfo(ai->ai_addr, ai->ai_addrlen,
-                  hbuf, sizeof hbuf,
-                  pbuf, sizeof pbuf,
-                  NI_NUMERICHOST|NI_NUMERICSERV);
-          if (!r) {
-              h = hbuf;
-              p = pbuf;
-          }
-          if (ai->ai_family == AF_INET6) {
-              printf("bind %d [%s]:%s\n", fd, h, p);
-          } else {
-              printf("bind %d %s:%s\n", fd, h, p);
-          }
-      }
       r = bind(fd, ai->ai_addr, ai->ai_addrlen);
       if (r == -1) {
         twarn("bind()");
         close(fd);
         continue;
+      }
+      if (verbose) {
+          char hbuf[NI_MAXHOST], pbuf[NI_MAXSERV], *h = host, *p = port;
+         struct sockaddr_in addr;
+         socklen_t addrlen;
+
+         addrlen = sizeof(addr);
+         r = getsockname(fd, (struct sockaddr *) &addr, &addrlen);
+         if (!r) {
+             r = getnameinfo((struct sockaddr *) &addr, addrlen,
+                             hbuf, sizeof(hbuf),
+                             pbuf, sizeof(pbuf),
+                             NI_NUMERICHOST|NI_NUMERICSERV);
+             if (!r) {
+                 h = hbuf;
+                 p = pbuf;
+             }
+         }
+          if (ai->ai_family == AF_INET6) {
+              printf("bind %d [%s]:%s\n", fd, h, p);
+          } else {
+              printf("bind %d %s:%s\n", fd, h, p);
+          }
       }
 
       r = listen(fd, 1024);
