f03fba3aeb984b65dce17646adeeb7a26523cdfd
Thomas Parrott <tomp@tomp.uk>
2019-06-29 14:48:59 +0100

    release 1.11

--- vers.sh
+++ vers.sh
@@ -1,11 +1,1 @@
-#!/bin/sh
-
-if git describe >/dev/null 2>&1
-then
-    git describe --tags --match=dev* | sed s/^dev// | tr - + | tr -d '\n'
-    if ! git diff --quiet HEAD
-    then printf +mod
-    fi
-else
-    printf unknown
-fi
+printf '1.11'
