ea040446fa6df58e80c2646cc64f56f02003c9c8
Yury Smolsky <yury@smolsky.by>
2019-07-16 09:16:41 +0300

    update the "ct" framework

--- ct/ct.c
+++ ct/ct.c
@@ -167,7 +167,7 @@ waittest(Test *ts)
     if (pid == -1) {
         die(3, errno, "wait");
     }
-    killpg(pid, 9);
+    killpg(pid, SIGKILL);
 
     for (t=ts; t->f; t++) {
         if (t->pid == pid) {
@@ -337,7 +337,7 @@ runbenchn(Benchmark *b, int n)
     if (pid == -1) {
         die(3, errno, "wait");
     }
-    killpg(pid, 9);
+    killpg(pid, SIGKILL);
     rmtree(b->dir);
     if (b->status != 0) {
         putchar('\n');
